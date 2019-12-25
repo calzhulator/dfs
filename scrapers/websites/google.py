eef68a87d64e73076f133bc3cbb7311294798351
calzhulator <cyz209@nyu.edu>
2019-12-24 20:58:45 -0500

    git v2

--- scrapers/websites/google.py
+++ scrapers/websites/google.py
@@ -1,10 +1,18 @@
 from googleapiclient.discovery import build
+from functools import lru_cache
 
 my_api_key = "AIzaSyDx8_U8dI9SyEMpWq2PM1Q6H1tQpB_W0c0"
 my_cse_id = "011876155113649707850:rwpmxdk0yms"
+overrides = {'profootballreference team ten': ['https://www.pro-football-reference.com/teams/oti/index.htm']}
 
 
-def first_link_result(text):
-    service = build("customsearch", "v1", developerKey=my_api_key)
-    res = service.cse().list(q=text, cx=my_cse_id).execute()
-    return res['items'][0]['link']
+@lru_cache(maxsize=None)
+def link_results(text):
+    if text in overrides.keys():
+        print("Using google override for: " + text)
+        return overrides[text]
+    else:
+        print("Googling results for: " + text)
+        service = build("customsearch", "v1", developerKey=my_api_key)
+        res = service.cse().list(q=text, cx=my_cse_id).execute()
+        return [it['link'] for it in res['items']]
