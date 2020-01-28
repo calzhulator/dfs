e835a3d8a6a44cd8c607ab6f94a9fb311da1752c
calzhulator <cyz209@nyu.edu>
2020-01-27 22:27:05 -0500

    git v3

--- scrapers/toolkit.py
+++ scrapers/toolkit.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 from functools import lru_cache
+import pandas as pd
+from selenium import webdriver
+driver = webdriver.Chrome()
 
 
 @lru_cache(maxsize=None)
@@ -8,3 +11,14 @@ def scrape(html):
     page = requests.get(html)
     html_code = BeautifulSoup(page.content, "lxml")
     return page, html_code
+
+
+@lru_cache(maxsize=None)
+def html_pandas(html):
+    table_list = pd.read_html(html)
+    return table_list
+
+
+def javascript_scraper(html):
+    driver.get(html)
+    return driver
